e52d265eb7addde220fc58bb2c3e184c547cd7b4
Joe <DiSalvoJoe@gmail.com>
2019-11-05 22:09:44 -0500

    Send draw messages

--- server/src/see_hear/core.clj
+++ server/src/see_hear/core.clj
@@ -1,6 +1,20 @@
-(ns see-hear.core)
+(ns see-hear.core
+  (:require [cheshire.core :as json]
+            [org.httpkit.server :as httpkit]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defonce channel (atom nil))
+(defonce stops (atom []))
+
+(defn websocket-handler [ring-request]
+  (httpkit/with-channel ring-request req-channel
+    (if (httpkit/websocket? req-channel)
+      (reset! channel req-channel)
+      (throw (Exception. "aaaalllll")))))
+
+(defn go
+  []
+  (swap! stops conj (httpkit/run-server websocket-handler {:port 8080})))
+
+(defn send!
+  [obj]
+  (httpkit/send! @channel (json/generate-string obj)))
